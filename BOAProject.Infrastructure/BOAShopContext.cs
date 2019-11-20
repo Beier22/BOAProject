80baed04b06e12a61a267ea11c0d2cfd1748731b
Szymon Soczewka <sz.soczewka@gmail.com>
2019-11-20 14:18:35 +0100

    + DbContext basic setup

---
+++ BOAProject.Infrastructure/BOAShopContext.cs
@@ -0,0 +1,33 @@
+﻿using BOAProject.Core.Entity;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BOAProject.Infrastructure
+{
+    class BOAShopContext : DbContext
+    {
+        public BOAShopContext(DbContextOptions<BOAShopContext> opt) : base(opt)
+        {}
+        public DbSet<User> Users { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Address> Addresses { get; set; }
+        public DbSet<Collection> Collections { get; set; }
+        public DbSet<Order> Orders { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+
+        }
+
+
+
+
+    }
+
+   
+   
+
+}
+
