828caa2521ad1470d4efc95f3b3e0e95aca43748
Szymon Soczewka <sz.soczewka@gmail.com>
2019-11-26 14:29:04 +0100

    New Entity

---
+++ BOAProject.Core.Entity/ProductQuantity.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BOAProject.Core.Entity
+{
+    public class ProductQuantity
+    {
+        public Product Product { get; set; }
+        public int Quantity { get; set; }
+    }
+}
